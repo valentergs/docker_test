436df772a9f9783ac08ee89cc972afae76e47793
Rodrigo Valente <rodrigovalente@hotmail.com>
2019-07-02 21:51:57 -0300

    mudei o app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Rodrigo Vasconcelos Valente</p>
+        <p>Mudei o app.js</p>
         <a
           className="App-link"
           href="https://reactjs.org"
